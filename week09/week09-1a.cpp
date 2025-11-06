985e508a7ff9d1847e52cc1e2f07cb32c822dfb7
13750032 <2006xiu@gmail.com>
2025-11-06 17:14:40 +0800

    week09

---
+++ week09/week09-1a.cpp
@@ -0,0 +1,14 @@
+///week09-1a.cpp
+///SOIT106_ADVACE_004
+#include <iostream>
+#include <cctype>///isupper() tolower()
+using namespace std;
+int main(){
+	char c;
+	while(cin>>c){
+		if(isupper(c))c=tolower(c);
+		else if(islower(c))c=toupper(c);
+		cout<<c;
+	}
+	cout<<"\n";
+}
